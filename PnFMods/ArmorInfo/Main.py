a472707e7ccf30599af8c1da8646f528e7681936
prurite <prurite@outlook.com>
2026-07-13 22:24:49 +0800

    Add configurable draggable armor info window

--- PnFMods/ArmorInfo/Main.py
+++ PnFMods/ArmorInfo/Main.py
@@ -1,16 +1,16 @@
-API_VERSION = 'API_v1.0'  # This is Mandatory!
-MOD_NAME = 'ArmorInfo'  # Your Mod Name
+API_VERSION = 'API_v1.0'
+MOD_NAME = 'ArmorInfo'
+
 from DataEnum import EnumX, EnumIX, EnumVIII, EnumEX, EnumVII, EnumVI, EnumV, EnumIV, EnumIII
 
 try:
-    import battle, events, dataHub, ui, constants, utils, callbacks
+    import battle, callbacks, events, ui
 except:
     pass
 
 
 class ArmorInfo(object):
     def __init__(self):
-        # self._testEnum = TestEnum
         self._enumX = EnumX
         self._enumIX = EnumIX
         self._enumVIII = EnumVIII
@@ -32,19 +32,30 @@ class ArmorInfo(object):
         self._uiId = None
 
     def onBattleStart(self, *args):
+        self._cancelTargetCallback()
+        if self._uiId is not None:
+            ui.deleteUiElement(self._uiId)
+
+        self._targetPlayerName = None
         self._uiId = uiId = ui.createUiElement()
         ui.addDataComponentWithId(uiId, 'ArmorInfoModEntityKey', {})
-        self._vary = callbacks.callback(0.1, self.getTargetShip)
         self._isObserver = battle.isObserverMode()
+        self._vary = callbacks.callback(0.1, self.getTargetShip)
 
     def onQuit(self, *args):
-        callbacks.cancel(self._vary)
-        self._vary = None
+        self._cancelTargetCallback()
         self._targetPlayerName = None
         self._cacheSet = {}
         self._cacheIDS = None
-        ui.deleteUiElement(self._uiId)
+        if self._uiId is not None:
+            ui.deleteUiElement(self._uiId)
+            self._uiId = None
         self._isObserver = False
+
+    def _cancelTargetCallback(self):
+        if self._vary is not None:
+            callbacks.cancel(self._vary)
+            self._vary = None
 
     def getTargetShip(self, *args, **kwargs):
         # GetObserverShipIDS reference DamageMonitor
@@ -56,35 +67,47 @@ class ArmorInfo(object):
         if target is None and not self._isObserver:
             target = battle.getSelfPlayerShip()
 
-        pName = target.playerName if target else None
-        if self._targetPlayerName != pName:
-            targetPlayerInfo = battle.getPlayerInfoByName(pName)
-            self._targetPlayerName = pName
-            targetShipIDS = targetPlayerInfo.shipInfo.shortName if pName else None
-            shipType = targetPlayerInfo.shipInfo.subtype if pName else None
-            shipLv = targetPlayerInfo.shipInfo.level if pName else None
+        playerName = target.playerName if target else None
+        if self._targetPlayerName == playerName:
+            return
 
-            if targetShipIDS == self._cacheIDS:
-                armorInfo = self._cacheSet.copy()
-            else:
-                getInfo = getattr(self.getEnumByLevel(shipLv, shipType), targetShipIDS, self._defSet)
-                processed_dict = {}
-                for key, value in getInfo.items():
-                    processed_dict[key] = str(value) if value and value != '0' else 'N/A'
-                armorInfo = processed_dict
-                self._cacheIDS = targetShipIDS
-                self._cacheSet = armorInfo.copy()
+        if playerName is None:
+            self._targetPlayerName = None
+            if self._uiId is not None:
+                ui.updateUiElementData(self._uiId, {})
+            return
 
+        targetPlayerInfo = battle.getPlayerInfoByName(playerName)
+        if targetPlayerInfo is None or targetPlayerInfo.shipInfo is None:
+            return
+
+        self._targetPlayerName = playerName
+        targetShipIDS = targetPlayerInfo.shipInfo.shortName
+        shipType = targetPlayerInfo.shipInfo.subtype
+        shipLv = targetPlayerInfo.shipInfo.level
+
+        if targetShipIDS == self._cacheIDS:
+            armorInfo = self._cacheSet.copy()
+        else:
+            getInfo = getattr(self.getEnumByLevel(shipLv, shipType), targetShipIDS, self._defSet)
+            processedInfo = {}
+            for key, value in getInfo.items():
+                processedInfo[key] = str(value) if value and value != '0' else 'N/A'
+            armorInfo = processedInfo
+            self._cacheIDS = targetShipIDS
+            self._cacheSet = armorInfo.copy()
+
+        if self._uiId is not None:
             ui.updateUiElementData(self._uiId, dict(
                 shipIDS='IDS_' + targetShipIDS,
                 shipType=shipType,
-                citadel=armorInfo["citadel"],
-                bow_st=armorInfo["bow_st"],
-                cas=armorInfo["cas"],
-                cas_deck=armorInfo["cas_deck"],
-                outer=armorInfo["outer"],
-                dd_cas=armorInfo["dd_cas"],
-                cas_t=armorInfo["cas_t"],
+                citadel=armorInfo['citadel'],
+                bow_st=armorInfo['bow_st'],
+                cas=armorInfo['cas'],
+                cas_deck=armorInfo['cas_deck'],
+                outer=armorInfo['outer'],
+                dd_cas=armorInfo['dd_cas'],
+                cas_t=armorInfo['cas_t'],
                 bow_st_s=armorInfo['bow_st_s'],
             ))
 
